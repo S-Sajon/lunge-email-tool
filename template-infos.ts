f42fdc34979c935d7d3e6fb6e6b49177187eca18
Sirajis Salekin Sajon <sirajis.sajon@selisegroup.com>
2024-12-12 03:33:38 +0600

    Updated templates

--- template-infos.ts
+++ template-infos.ts
@@ -1,16 +1,25 @@
 export const EmailTemplateInfos: {
   [key: string]: { name: string; subject: string };
 } = {
-  ApproveCleanUpRequest: {
-    name: 'ApproveCleanUpRequest',
-    subject: 'A cleanup request has been approved.',
+  // ApproveCleanUpRequest: {
+  //   name: 'ApproveCleanUpRequest',
+  //   subject: 'A cleanup request has been approved.',
+  // },
+  // RejectCleanUpRequest: {
+  //   name: 'RejectCleanUpRequest',
+  //   subject: 'A cleanup request has been rejected.',
+  // },
+  // ResentCleanUpRequest: {
+  //   name: 'ResentCleanUpRequest',
+  //   subject: 'A cleanup request has been resent.',
+  // },
+  PlaDataOrderSubmissionOpen: {
+    name: 'PlaDataOrderSubmissionOpen',
+    subject: 'A PLA Data Order is available for data submission.',
   },
-  RejectCleanUpRequest: {
-    name: 'RejectCleanUpRequest',
-    subject: 'A cleanup request has been rejected.',
-  },
-  ResentCleanUpRequest: {
-    name: 'ResentCleanUpRequest',
-    subject: 'A cleanup request has been resent.',
+  PlaDataOrderProjectsDeliveredToSwisslife: {
+    name: 'PlaDataOrderProjectsDeliveredToSwisslife',
+    subject:
+      'A Network Partner has delivered project attribute data to SwissLife.',
   },
 };
